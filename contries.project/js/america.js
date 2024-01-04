31c275508ac6b5f75c3d1a4923f72f37a3b8745d
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-01-04 17:25:13 +0200

    commit

---
+++ contries.project/js/america.js
@@ -0,0 +1,16 @@
+const mainDiv=document.querySelector('#mainDiv');
+const fettchCountries=(countryName='America')=> {
+
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then(res=>{return res.json()})
+    .then(data=>{
+        console.log(data);
+        mainDiv.innerHTML+=`<h1>${data[0].name.common}</h1>
+        <img src="${data[0].flags.png}">
+        <p>${data[0].capital}</p>
+        <p>${data[0].borders}</p>
+        <iframe src="https://maps.google.com/maps?q=${data[0].latlng}&z=8&ie=UTF8&iwloc=&output=embed" target="_parent" ></iframe>`
+    })
+    .catch(error=>{console.log(error)})
+}
+fettchCountries()
---
+++ contries.project/js/main.js
@@ -0,0 +1,34 @@
+const mainDiv=document.querySelector('#mainDiv');
+const fettchCountries=(countryName='Israel')=> {
+
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then(res=>{return res.json()})
+    .then(data=>{
+        console.log(data);
+        mainDiv.innerHTML = `
+        <h1>${data[0].name.common}</h1>
+        <div class="country-info">
+            <img src="${data[0].flags.png}" alt="Flag">
+            <p><strong>Capital city:</strong> ${data[0].capital}</p>
+            <p><strong>Borders Country:</strong> ${data[0].borders}</p>
+        </div>
+        <div class="map-container">
+            <iframe src="https://maps.google.com/maps?q=${data[0].latlng}&z=8&ie=UTF8&iwloc=&output=embed" target="_parent"></iframe>
+        </div>`
+    })
+    .catch(error=>{console.log(error)})
+}
+fettchCountries()
+
+const rndBtn=document.querySelector('#rndBtn');
+rndBtn.addEventListener('click', ()=>{
+    const rndNum=Math.floor(Math.random()*250);
+    fetch(`https://restcountries.com/v3.1/all`)
+    .then(res=>{return res.json()})
+    .then(data=>{
+        console.log(data);
+        fettchCountries(data[rndNum].name.common);
+    })
+    .catch(error=>{console.log(error)})
+    
+})
